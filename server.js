23c0a0d1286a91136a72808cc3d0ebc6c6e15230
Anshulsantoshi <santoshi2005anshul@gmail.com>
2025-04-27 17:14:30 +0530

    finaly top tracks fake events

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
